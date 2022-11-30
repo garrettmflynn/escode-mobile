491522373f32341d79d0ddd0f004a5e7d4296166
Garrett <gflynn@usc.edu>
2022-11-29 18:26:47 -0800

    Just bare

--- index.esc.js
+++ index.esc.js
@@ -2,7 +2,8 @@ export const __children = {
     header: {
         __element: 'h1',
         __attributes: {
-            innerText: 'Hello (no esmpile, no editor usage)'
+            innerText: 'Hello (bare)'
+            // innerText: 'Hello (no esmpile, no editor usage)'
         }
     }
 }
