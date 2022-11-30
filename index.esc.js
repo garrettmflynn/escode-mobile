4a235acec36520bda36a432f4b923f3897f1a622
Garrett <gflynn@usc.edu>
2022-11-29 18:18:48 -0800

    From text

--- index.esc.js
+++ index.esc.js
@@ -2,7 +2,7 @@ export const __children = {
     header: {
         __element: 'h1',
         __attributes: {
-            innerText: 'Hello with Editor (no import)'
+            innerText: 'Hello with Text (no editor)'
         }
     }
 }
