f2bcdc1c6a17e137252273cf1332bb2fbe6b35e6
Garrett <gflynn@usc.edu>
2022-11-29 18:09:16 -0800

    Update index.esc.js

--- index.esc.js
+++ index.esc.js
@@ -2,7 +2,7 @@ export const __children = {
     header: {
         __element: 'h1',
         __attributes: {
-            innerText: 'Hello'
+            innerText: 'Hello from Text'
         }
     }
 }
